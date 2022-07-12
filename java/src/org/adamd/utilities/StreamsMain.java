bbfb1f19378391914769947d306bcaeea2da656e
stackrev <farbitherast5888@gmail.com>
2022-07-12 20:04:56 +0200

    Streams

--- java/src/org/adamd/utilities/StreamsMain.java
+++ java/src/org/adamd/utilities/StreamsMain.java
@@ -1,4 +1,29 @@
 package org.adamd.utilities;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * Here I want to show how to navigate through an array of heterogenous types - usually an effect of a generic or
+ * templated list.
+ * Using parallel streams and spliterators I will count the total of an array of integers, floats (will floor these)
+ * and strings.
+ *
+ * @author adamd1985
+ */
 public class StreamsMain {
+
+    public static void main(String[] args) {
+        Object[] arrayObjs = {1, "2", 3, "4", "5", 6};
+
+        var result = Stream.of(arrayObjs).parallel().flatMap(
+                        obj -> (obj instanceof String) ?
+                                Stream.of(Integer.valueOf((String)obj)) :
+                                Stream.of((Integer)obj)
+                ).reduce(0, (a, b) -> a + b);
+
+        System.out.println(result);
+    }
 }
